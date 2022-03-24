613b90b738790a86401baacd7108dd5f63b23017
MohibulAlamjr <mohibulalamjr@gmail.com>
2022-03-24 23:45:13 +0600

    new

--- stamford-university-bangladesh/src/components/navigation/Topnavigation.js
+++ stamford-university-bangladesh/src/components/navigation/Topnavigation.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
-import {Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col,Nav } from 'react-bootstrap';
+
 
 export default class Topnavigation extends Component {
   render() {
     return (
       <div>
+        
           <Container fluid>
   <Row className='topNavbg'>
-    <Col>1 of 1</Col>
+            <Col>
+              <Container >
+                <Row>
+                  <Col>
+                    <Nav
+                      className='anchorDiv'
+                      activeKey="/home"
+                      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+                    >
+  <Nav.Item>
+    <Nav.Link className='anchorDiv' href="/home">admission@stamford.university</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link className='anchorDiv' eventKey="link-1">DEGREE VERIFICATION</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link className='anchorDiv' eventKey="link-2">Library</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link className='anchorDiv'  eventKey="link-3" >IQAC</Nav.Link>
+  </Nav.Item>
+                      
+</Nav>
+                  </Col>
+                  
+                </Row>
+              </Container>
+            
+            </Col>
   </Row>
 </Container>
       </div>
