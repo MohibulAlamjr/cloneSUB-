8329aaa83267737993be8766412758399fd49f41
ShovhanaSweety <sweetycse.engr@gmail.com>
2022-04-03 23:42:14 +0600

    Last

--- stamford-university-bangladesh/src/components/navigation/Midnavigation.js
+++ stamford-university-bangladesh/src/components/navigation/Midnavigation.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
 import { Container, Row, Col,Button,Form,FormControl } from 'react-bootstrap';
-import stamfordLogo from '../../image/logo.png';
-import sekhMujiburRahman from'../../'
+import StamfordLogo from '../../image/logo.png';
+import SekhMujiburRahman from '../../image/mujib.png';
+import Subornojoyonti from '../../image/suborno_50.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faFac } from '@fortawesome/free-solid-svg-icons';
+
 
 export default class Midnavigation extends Component {
   render() {
@@ -12,9 +16,9 @@ export default class Midnavigation extends Component {
                     <Col>
                     <Container>
                         <Row>
-                                <Col  lg='5'> <img src={stamfordLogo} alt='logo' /> </Col>
+                                <Col  lg='4'> <img className="stamford" src={StamfordLogo} alt='logo' /> </Col>
                                 <Col  lg='2'><Button className='midNavButton'>Online Admission</Button></Col>
-                                <Col lg='1'>
+                                <Col lg='2'>
                                     <Form className="d-flex">
                                     <FormControl
                                         type="search"
@@ -26,7 +30,10 @@ export default class Midnavigation extends Component {
                                     </Form>
                                 </Col>
                                 <Col lg='4'>
-                                    
+                                <i><FontAwesomeIcon icon="fa-brands fa-facebook-f" /></i>
+                                   <img className="suborno" src={Subornojoyonti} alt='Sekh' />
+                                   <img className="sekhMujib" src={SekhMujiburRahman} alt='Sekh' />
+                                   
                                 </Col>
                         </Row>
                     </Container>
